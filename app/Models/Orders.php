18bfb111e2341f2862084dbaf735b7629864092b
Diego Viveros <diegoviveros.m@gmail.com>
2025-11-27 16:30:43 -0300

    fix renombre HasFactory

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 // Para utilizar factories en el modelo se debe invocar e injectar
-use Illuminate\Database\Eloquent\Factories\hasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Orders extends Model 
